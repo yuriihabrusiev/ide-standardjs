368e363bddd98932f62d293adbfd7f06e44ff349
Yurii Habrusiev <yuriihabrusiev@gmail.com>
2017-11-24 13:21:56 +0200

    Initial commit

---
+++ spec/ide-standardjs-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import IdeStandardjsView from '../lib/ide-standardjs-view';
+
+describe('IdeStandardjsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
